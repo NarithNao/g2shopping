b67ecea3a73aeb0661b0edabc8c5638cb2d58a12
NarithNao <narithnao@gmail.com>
2016-07-24 18:03:02 -0700

    commit

---
+++ database/migrations/2016_07_24_184807_create_location_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('user_id')->unsigned()->index();
+            $table->string('country', '100');
+            $table->string('city', '100');
+            $table->string('address', '300');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locations');
+    }
+}
---
+++ database/migrations/2016_07_24_185046_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('user_type_id')->unsigned()->index();
+            $table->string('username', '20');
+            $table->string('email', '100');
+            $table->string('password', '100');
+            $table->string('firstname', '20');
+            $table->string('lastname', '20');
+            $table->string('country', '50');
+            $table->string('city', '50');
+            $table->string('address', '100');
+            $table->string('phone', '20');
+            $table->date('last_activity');
+            $table->tinyInteger('newsletter');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
